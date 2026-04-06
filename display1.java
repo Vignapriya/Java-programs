b20e08a7f026e08d58e6558a84fcae01aba6ea0a
Vignapriya <vigna1419@gmail.com>
2026-04-06 11:55:46 +0530

    display1.java

---
+++ display1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class main
+{
+    public static void main(String[] args)
+    {
+        Scanner scan=new Scanner(System.in);
+        int a=scan.nextInt();
+        System.out.print("Number of events hosted in Codissia is "+a);
+    }
+    }// You are using Java
