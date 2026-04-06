801fececdf502cf82ef6fade0ff53eae1162f15c
Vignapriya <vigna1419@gmail.com>
2026-04-06 12:04:33 +0530

    ticket.java

---
+++ ticket.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class main
+{
+    public static void main(String[] args)
+    {
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        if(n<15)
+        {
+            System.out.print("Child Ticket");
+        }
+        else 
+        {
+            System.out.print("Adult Ticket");
+        }
+    }// You are using Java
+}
