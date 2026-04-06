61b81c61943150770877f6adf70a8444565f0593
Vignapriya <vigna1419@gmail.com>
2026-04-06 11:55:19 +0530

    display.java

---
+++ display.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class main
+{
+    public static void main(String[] args)
+    {
+        Scanner scan=new Scanner(System.in);
+        String name=scan.nextLine();
+        int age=scan.nextInt();
+        System.out.print("Age of "+name+" is "+age);
+    }// You are using Java
+}
