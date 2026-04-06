bbe7f5c4cb40c76196b202ba69f802d73142589b
Vignapriya <vigna1419@gmail.com>
2026-04-06 11:54:52 +0530

    add.java

---
+++ add.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class main
+{
+    public static void main(String[] args)
+    {
+        Scanner scan=new Scanner(System.in);
+        int x=scan.nextInt();
+        int y=scan.nextInt();
+        double z=x+y;
+        System.out.print(z);
+    }
+    }// You are using Java
