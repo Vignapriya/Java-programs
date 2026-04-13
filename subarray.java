d73efc9565d41d6f4a4991d012b5e9f1f21a7588
Vignapriya <vigna1419@gmail.com>
2026-04-13 11:07:24 +0530

    subarray.java

---
+++ subarray.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] a = new int[n];
+        
+        for(int i = 0; i < n; i++) {
+            a[i] = scan.nextInt();
+        }
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            int currentSum = 0;
+            for (int j = i; j < n; j++) {
+                currentSum += a[j];
+                if (currentSum < 0) {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
